ea420742070cf4775f54c328c6f7b8872a40967a
Pius Kariuki <engkarishpius10@gmail.com>
2023-02-25 00:50:43 +0300

    First commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,10 +7,10 @@ const Header = () => {
       <img src="https://www.shutterstock.com/image-vector/digital-technology-logo-template-mobile-600w-1029483697.jpg" alt="Logo" className="logo" />
       <nav>
         <ul>
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Blogs</a></li>
-          <li><a href="#">Contact</a></li>
+          <li><a href="https://amzn.to/3ITCzF6">Home</a></li>
+          <li><a href="https://amzn.to/3SrVES4">About</a></li>
+          <li><a href="https://blog.aboutamazon.com/?utm_source=gateway&utm_medium=footer">Blogs</a></li>
+          <li><a href="https://affiliate-program.amazon.com/home/contact/ref=amb_link_4s-gyt_BNmi8uXuh9NeWSg_10?pf_rd_p=c705084d-dd5f-4deb-9368-305ad6e10000&_encoding=UTF8%2F">Contact</a></li>
         </ul>
       </nav>
       <button className="cta">Get Started</button>
