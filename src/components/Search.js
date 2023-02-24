966a5ed7dd8477bdaa4148134a696fc03cca204b
Pius Kariuki <engkarishpius10@gmail.com>
2023-02-25 00:56:11 +0300

    First commit

--- src/components/Search.js
+++ src/components/Search.js
@@ -5,14 +5,14 @@ function Search() {
   return (
     <div className="search-container">
       
-      <img className='man' src="https://assets.jiji.co.ke/static/img/main-page/man.png"/>
+      <img className='man' src="https://assets.jiji.co.ke/static/img/main-page/man.png" alt=''/>
       <div className='call-to-search'>
         <p>Find your best Smart Device here</p>
         <input type="text" placeholder="Search..." />
       </div>
       
       
-      <img className='girls' src='https://assets.jiji.co.ke/static/img/main-page/girls.png' />
+      <img className='girls' src='https://assets.jiji.co.ke/static/img/main-page/girls.png' alt='' />
     </div>
   );
 }
